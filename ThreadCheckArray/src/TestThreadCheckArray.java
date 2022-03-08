432b8a220aa1edd4d4bd8981b1ed8673093271f8
haniz1999 <hanizahran3@gmail.com>
2022-03-08 17:27:38 +0200

    ewtrw

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -6,7 +6,7 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			int [] array = new int[num+1];
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
